38ba9fd150db2d84bf334052d147b0ff9a49e460
theduynguyen <theduynguyen2001@gmail.com>
2023-05-17 01:15:26 +1000

    client: init VM prompt implemented

--- client-server/client.c
+++ client-server/client.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <getopt.h>
 
 #include "rpc.h"
 
@@ -14,27 +15,66 @@
  * Main entry to client program.
  * @return 0 if successful
  */
-int main(int argc, char *argv[]) {
-    int exit_code = 0;
+int main(int argc, char** argv) {
 
-    rpc_client* state = rpc_init_client("::1", 3000);
+    // parse the arguments
+    int MIN_ARGC = 4;
+    if (argc < MIN_ARGC) {
+        fprintf(stderr, "client: insufficient arguments\n");
+        exit(EXIT_FAILURE);
+    }
+    // flags
+    char* ip   = NULL;
+    char* port = NULL;
+    int c;
+
+    // check input flags
+    while ((c = getopt(argc, argv, "i:p:")) != -1) {
+        switch (c) {
+            case 'i':
+                ip = optarg;
+                break;
+            case 'p':
+                port = optarg;
+                break;
+            case '?':
+                fprintf(stderr, "client: error in option -%c. Aborting...\n", optopt);
+            default:
+                exit(1);
+        }
+    }
+    // check if any flag is left empty
+    if (ip == NULL) {
+        fprintf(stderr, "client: no -i flag for IP address found\n");
+        exit(EXIT_FAILURE);
+    }
+    if (port == NULL) {
+        fprintf(stderr, "client: no -p flag for port number found\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // convert port to integer to initialize client RPC
+    int port_num = atoi(port); // NOLINT(cert-err34-c)
+    int exit_code = 0;
+    rpc_client* state = rpc_init_client(ip, port_num);
     if (state == NULL)
         exit(EXIT_FAILURE);
 
+    // find function on server
     rpc_handle* handle_add2 = rpc_find(state, "add2");
     if (handle_add2 == NULL) {
-        fprintf(stderr, "ERROR: Function add2 does not exist\n");
+        fprintf(stderr, "client: function add2 does not exist\n");
         exit_code = 1;
         goto cleanup;
     }
 
     for (int i = 0; i < 2; i++) {
-        /* Prepare request */
+        // Prepare request
         char left_operand = (char) i;
         char right_operand = 100;
         rpc_data request_data = {.data1 = left_operand, .data2_len = 1, .data2 = &right_operand};
 
-        /* Call and receive response */
+        // Call and receive response
         rpc_data* response_data = rpc_call(state, handle_add2, &request_data);
         if (response_data == NULL) {
             fprintf(stderr, "Function call of add2 failed\n");
@@ -42,7 +82,7 @@ int main(int argc, char *argv[]) {
             goto cleanup;
         }
 
-        /* Interpret response */
+        // Interpret response
         assert(response_data->data2_len == 0);
         assert(response_data->data2 == NULL);
         printf("Result of adding %d and %d: %d\n", left_operand, right_operand,
@@ -50,6 +90,7 @@ int main(int argc, char *argv[]) {
         rpc_data_free(response_data);
     }
 
+    // cleanup handle and RPC
 cleanup:
     if (handle_add2 != NULL)
         free(handle_add2);
